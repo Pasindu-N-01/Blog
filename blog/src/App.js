c0f0db665da7efdea3d7cd71cff18a570f23129c
Pasindu-N-01 <pasindunishshanka82@gmail.com>
2023-01-10 23:56:43 +0530

    updated

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,9 +3,10 @@ import TopBar from "./TopBar/TopBar";
 
 function App() {
   return (
-    
+      <>
       <TopBar />
-      
+      <p>Hello</p>
+      </>
   );
 }
 
--- blog/src/TopBar/TopBar.jsx
+++ blog/src/TopBar/TopBar.jsx
@@ -1,9 +1,30 @@
 import './TopBar.css'
 
+
+
 export default function TopBar() {
   return (
     <div className='top'>
-      topbar
+      <div className="topLeft"><i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-square-whatsapp"></i>
+      <i class="fa-brands fa-square-instagram"></i>
+      <i class="fa-brands fa-linkedin"></i>
+    </div>
+
+      <div className="topCenter">
+        <div className="topList">
+            <li className='topListItems'>Home</li>
+            <li className='topListItems'>About & Contact</li>
+            <li className='topListItems'>Blog</li>
+            <li className='topListItems'>LogOut</li>
+        </div>
+      </div>
+
+      <div className="topRight">
+        <img src="./images/p6.jpg" alt="" />
+      </div>
+      
+
     </div>
   )
 }
